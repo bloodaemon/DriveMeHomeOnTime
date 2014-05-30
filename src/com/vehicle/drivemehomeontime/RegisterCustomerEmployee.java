7b5075fa2ec1f82e96a298587bcaf5e83308f0df
bloodaemon <richardszeto74@gmail.com>
2014-05-29 18:33:39 -0700

    v0.01a Combined Congocash and gituser5 parital work. changed id and string names to work with namespace.

--- src/com/vehicle/drivemehomeontime/RegisterCustomerEmployee.java
+++ src/com/vehicle/drivemehomeontime/RegisterCustomerEmployee.java
@@ -3,14 +3,14 @@ package com.vehicle.drivemehomeontime;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class System_Menu extends Activity
+public class RegisterCustomerEmployee extends Activity
 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_system__menu);
+        setContentView(R.layout.activity_register_customer_employee);
     }
 
 }
