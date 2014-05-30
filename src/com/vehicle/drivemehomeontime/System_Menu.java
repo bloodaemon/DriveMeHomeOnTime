1bc7dc46aead3583a820620ef1887861253c19b8
bloodaemon <richardszeto74@gmail.com>
2014-05-29 23:26:36 -0700

    v0.02a Used intent between System_Menu and RegisterCustomerEmployee changed Spinners to be dialog to display prompt

--- src/com/vehicle/drivemehomeontime/System_Menu.java
+++ src/com/vehicle/drivemehomeontime/System_Menu.java
@@ -1,7 +1,9 @@
 package com.vehicle.drivemehomeontime;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class System_Menu extends Activity
 {
@@ -11,6 +13,17 @@ public class System_Menu extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_system__menu);
+    }
+    
+    public void onClick(View v)
+    {
+        switch(v.getId())
+        {
+            case R.id.register_System_Menu:
+                Intent i = new Intent(getApplicationContext(), 
+                        RegisterCustomerEmployee.class);
+                startActivity(i);
+        }
     }
 
 }
