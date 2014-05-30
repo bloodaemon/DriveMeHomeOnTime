c5453221ac9285ec54d2f2388212e7e3201f79a9
bloodaemon <richardszeto74@gmail.com>
2014-05-29 19:00:13 -0700

    v0.02a changed radio group to spinner for driver_type

--- src/com/vehicle/drivemehomeontime/RegisterCustomerEmployee.java
+++ src/com/vehicle/drivemehomeontime/RegisterCustomerEmployee.java
@@ -2,15 +2,46 @@ package com.vehicle.drivemehomeontime;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Spinner;
 
 public class RegisterCustomerEmployee extends Activity
 {
+    private Spinner driver_spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_customer_employee);
+
+        addListenerOnSpinnerItemSelection();
+    }
+
+    public void addListenerOnSpinnerItemSelection()
+    {
+        driver_spinner = (Spinner) findViewById(R.id.driver_spinner_Register_Customer_Employee);
+        driver_spinner
+                .setOnItemSelectedListener(new CustomOnItemSelectedListener());
+    }
+
+    public class CustomOnItemSelectedListener implements
+            OnItemSelectedListener
+    {
+        @Override
+        public void onItemSelected(AdapterView<?> parent,
+                View view, int pos, long id)
+        {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> arg0)
+        {
+            // TODO Auto-generated method stub
+        }
     }
 
 }
